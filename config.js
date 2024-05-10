6cb30bb3eac2283305d938ca21decbab9b576e4b
manish <manish25.mv@gmail.com>
2024-05-10 16:10:05 -0700

    backend

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb://manish25mv:vzTQcykNkoO8FdZz@ac-kucux6c-shard-00-00.sywg04f.mongodb.net:27017,ac-kucux6c-shard-00-01.sywg04f.mongodb.net:27017,ac-kucux6c-shard-00-02.sywg04f.mongodb.net:27017/?ssl=true&replicaSet=atlas-zuwetw-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
